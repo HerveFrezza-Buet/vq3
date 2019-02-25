5603007ed769583ce0af70751773feb55c6f10ad
Herve Frezza-Buet <herve.frezza-buet@centralesupelec.fr>
2019-02-25 22:51:13 +0100

    astar

--- vq3/src/vq3ShortestPath.hpp
+++ vq3/src/vq3ShortestPath.hpp
@@ -46,10 +46,12 @@ namespace vq3 {
       status state;                 //!< Tells whether best path is found for this node.
       std::shared_ptr<void> to_src; //!< The ref edge leading to the source.
       double cost;                  //!< The cumulated cost.
+      double hcost;                 //!< The cumulated cost plus the heuristics.
       
       void raz() {
 	to_src = nullptr;
 	cost   = std::numeric_limits<double>::max();
+	hcost  = std::numeric_limits<double>::max();
 	state  = status::unprocessed;
       }
 
@@ -66,12 +68,34 @@ namespace vq3 {
 	state  = status::processing;
       }
 
+      template<typename REF_VERTEX>
+      void set(double new_val, double estimation_to_dest, const REF_VERTEX& parent_edge) {
+	to_src = parent_edge;
+	cost   = new_val;
+	hcost  = new_val + estimation_to_dest;
+	state  = status::processing;
+      }
+
+      template<typename REF_VERTEX>
+      void set_estimated(double new_val, const REF_VERTEX& parent_edge) {
+	double h = hcost - cost;
+	to_src = parent_edge;
+	cost   = new_val;
+	hcost  = new_val + h;
+	// This is used for vertices which are already in status::processing state. 
+      }
+
+      void set(double new_val, double estimation_to_dest) {
+	to_src = nullptr;
+	cost   = new_val;
+	hcost  = new_val + estimation_to_dest;
+	state  = status::processing;
+      }
+
       void ended() {
 	state = status::done;
       }
 
-      bool operator<(const Info& other) {return cost < other.cost;}
-      
       Info() {raz();}
       Info(const Info&)            = default;
       Info& operator=(const Info&) = default;
@@ -236,7 +260,7 @@ namespace vq3 {
       g.foreach_vertex([](typename GRAPH::ref_vertex ref_v){(*ref_v)().vq3_shortest_path.raz();});
 
       auto accum_comp = [](const typename GRAPH::ref_vertex& ref_v1,
-			   const typename GRAPH::ref_vertex& ref_v2) {return (*ref_v1)().vq3_shortest_path < (*ref_v2)().vq3_shortest_path;};
+			   const typename GRAPH::ref_vertex& ref_v2) {return (*ref_v1)().vq3_shortest_path.cost < (*ref_v2)().vq3_shortest_path.cost;};
       std::set<typename GRAPH::ref_vertex, decltype(accum_comp)> q(accum_comp);
 
       if constexpr(VERTEX_EFFICIENCY) {
@@ -300,6 +324,97 @@ namespace vq3 {
 	  });
       }
     }
+
+    /**
+     * This function fills the vq3_shortest_path values at each vertex
+     * according to the shortest path linking each vertex to the
+     * destination vertex. The boolean template parameters
+     * VERTEX_EFFICIENCY and EDGE_EFFICIENCY toggle the efficiency
+     * test on vertices and edges (see the efficiency decorator).
+     * @param g the graph
+     * @param start the start vertex. Provide nullptr for computing shortest path from all the vertices to dest.
+     * @param dest the destination vertex
+     * @param edge_cost a function such as edge_cost(ref_edge) is the cost of the edge.
+     * @param to_start_estimation a function such as to_dest_estimation(ref_vertex) gives a pessimistic optimation of the cumulated cost from ref_vertex to start.
+     */
+    template<bool VERTEX_EFFICIENCY, bool EDGE_EFFICIENCY, typename GRAPH, typename EDGE_COST, typename TO_START>
+    void a_star(GRAPH& g, typename GRAPH::ref_vertex& start, typename GRAPH::ref_vertex& dest,
+		const EDGE_COST& edge_cost,
+		const TO_START& to_start_estimation) {
+      // Init
+      g.foreach_vertex([](typename GRAPH::ref_vertex ref_v){(*ref_v)().vq3_shortest_path.raz();});
+
+      auto accum_comp = [](const typename GRAPH::ref_vertex& ref_v1,
+			   const typename GRAPH::ref_vertex& ref_v2) {return (*ref_v1)().vq3_shortest_path.hcost < (*ref_v2)().vq3_shortest_path.hcost;};
+      std::set<typename GRAPH::ref_vertex, decltype(accum_comp)> q(accum_comp);
+
+      if constexpr(VERTEX_EFFICIENCY) {
+	  if((*dest)().vq3_efficient) {
+	    (*dest)().vq3_shortest_path.set(0, to_start_estimation(dest));
+	    q.insert(dest);
+	  }
+	  else
+	    return;
+	}
+      else {
+	(*dest)().vq3_shortest_path.set(0, to_start_estimation(dest));
+	q.insert(dest);
+      }
+      
+      while(!q.empty()) {
+	auto curr = *(q.begin());
+	auto& curr_path_info = (*curr)().vq3_shortest_path;
+	curr_path_info.ended();
+	if(curr == start) break;
+	
+	q.erase(q.begin());
+	curr->foreach_edge([curr, &edge_cost, &to_start_estimation, &q, &curr_path_info](typename GRAPH::ref_edge ref_e) {
+	    auto extr_pair = ref_e->extremities();           
+	    if(vq3::invalid_extremities(extr_pair)) {
+	      ref_e->kill();
+	      return;
+	    }
+
+	    if constexpr(EDGE_EFFICIENCY) {
+		if(!(*ref_e)().vq3_efficient)
+		  return;
+	      }
+			  
+	    double cost           = edge_cost(ref_e);
+	    auto& other           = vq3::other_extremity(extr_pair, curr);
+	    auto& other_path_info = (*other)().vq3_shortest_path;
+
+	    if constexpr(VERTEX_EFFICIENCY) {
+		if(!(*other)().vq3_efficient) {
+		  other_path_info.ended();
+		  return;
+		}
+	      }
+	    
+	    switch(other_path_info.state) {
+	    case status::done :
+	      break;
+	    case status::unprocessed :
+	      other_path_info.set(cost + curr_path_info.cost, to_start_estimation(other), ref_e);
+	      q.insert(other);
+	      break;
+	    case status::processing :
+	      if(double cost_candidate = cost + curr_path_info.cost; cost_candidate < other_path_info.cost) {
+		q.erase(other);
+		other_path_info.set_estimated(cost_candidate, ref_e);
+		q.insert(other);
+	      }
+	      break;
+	    }
+	  });
+      }
+    }
+
+
+
+
+
+    
   }
 
   
